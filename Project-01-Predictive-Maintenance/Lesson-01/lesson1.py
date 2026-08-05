d887cad953218ec1e79f63ec830f3d69b5f531a5
Judith Chisom Wilson <88676018+willblazer@users.noreply.github.com>
2026-08-05 08:22:22 +0100

    Add files via upload
    Complete Lesson 1: Python Basics

---
+++ Project-01-Predictive-Maintenance/Lesson-01/lesson1.py
@@ -0,0 +1,17 @@
+# a simple instrusction to print out my name
+print("Judith Chisom Wilson")
+# prints the name of my school
+print("University of Hertfordshire")
+# assigning my course of study to a variable and printing it
+course = "Artificial Intelligence and Robotics"
+print(course)
+# assigning my name,age ,and country to a variable 
+name = "judith Chisom Wilson"
+age =32
+country ="United Kingdom"
+# printing out the stored variables
+print(f"Name:{name}\nAge:{age}\nCountry:{country}") 
+# store my city
+city = "Hatfied"
+# printing out my city and country
+print(f"I live in {city}, {country}") 
